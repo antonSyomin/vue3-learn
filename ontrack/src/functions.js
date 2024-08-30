465cec48c35b14412583a2073607a678d05d1228
anton <seminanton2@gmail.com>
2024-08-30 18:11:26 +0300

    Генерация данных временной шкалы

--- ontrack/src/functions.js
+++ ontrack/src/functions.js
@@ -1,4 +1,5 @@
 import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from './constants'
+import {HOURS_IN_DAY} from './constants'
 
 export function normailzePageHash() {
     const hash = window.location.hash.slice(1);
@@ -9,4 +10,15 @@ export function normailzePageHash() {
 
     window.location.hash = PAGE_TIMELINE
     return PAGE_TIMELINE
+}
+
+export function generateTimelineItems()
+{
+    const timelineItems = []
+
+    for (let hour = 0; hour < HOURS_IN_DAY; hour++) {
+        timelineItems.push({ hour })
+    }
+
+    return timelineItems
 }
