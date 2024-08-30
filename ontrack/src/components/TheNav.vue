f9f722e3dbc4f14cbe83f14a8dd79ab64d7c3270
anton <seminanton2@gmail.com>
2024-08-30 14:48:36 +0300

    Компоненты vue

---
+++ ontrack/src/components/TheNav.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
+
+    const navItems = {
+        timeline: ClockIcon, 
+        activities: ListBulletIcon, 
+        progress: ChartBarIcon
+    }
+</script>
+<template>
+    <nav class='sticky bottom-0 bg-white z-10 text-3xl'>
+        <ul class='flex items-center justify-around border-t'>
+        <li  v-for='icon, page in navItems' :key='page' class='flex-1'>
+            <a :href="`#${page}`" class='flex flex-col items-center p-2 text-xs capitalize'>
+            <component :is='icon' class='h-6 w-6'/> {{ page }}
+            </a>
+        </li>
+        </ul>
+    </nav>
+</template>
