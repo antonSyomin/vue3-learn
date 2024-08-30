9c8b79b06806123a2b106b288929edc5c3f40890
anton <seminanton2@gmail.com>
2024-08-30 14:27:37 +0300

    Динамический компонент component

--- ontrack/src/App.vue
+++ ontrack/src/App.vue
@@ -2,7 +2,11 @@
   import {CheckCircleIcon} from '@heroicons/vue/24/solid'
   import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
 
-  const navItems = ['timeline', 'activities', 'progress']
+  const navItems = {
+    timeline: ClockIcon, 
+    activities: ListBulletIcon, 
+    progress: ChartBarIcon
+  }
 </script>
 <template>
   <header class='sticky top-0 z-20 flex justify-between border-b bg-white p-3'>
@@ -24,11 +28,13 @@
   </main>
   <nav class='sticky bottom-0 bg-white z-10 text-3xl'>
     <ul class='flex items-center justify-around border-t'>
-      <li  v-for='page in navItems' :key='page' class='flex-1'>
+      <li  v-for='icon, page in navItems' :key='page' class='flex-1'>
         <a :href="`#${page}`" class='flex flex-col items-center p-2 text-xs capitalize'>
+        <component :is='icon' class='h-6 w-6'/> {{ page }}
+        <!--
           <ClockIcon v-if='page === "activities"' class='h-6 w-6'/>
           <ListBulletIcon v-else-if='page === "timeline"' class='h-6 w-6'/>
-          <ChartBarIcon v-else='page === "progress"' class='h-6 w-6'/> {{ page }}
+          <ChartBarIcon v-else='page === "progress"' class='h-6 w-6'/> {{ page }}-->
         </a>
       </li>
     </ul>
