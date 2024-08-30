f78b15ec7ee9a4d170cd216c2f11ec4807f5ac37
anton <seminanton2@gmail.com>
2024-08-30 19:02:09 +0300

    Добавил компонент TimelineItem

--- ontrack/src/pages/TheTimeline.vue
+++ ontrack/src/pages/TheTimeline.vue
@@ -1,21 +1,18 @@
 <script setup>
+    import TimelineItem from '../components/TimelineItem.vue'
+    import { generateTimelineItems } from '../functions.js'
 
-import { generateTimelineItems } from '../functions.js'
-
-const timelineItems = generateTimelineItems()
-
+    const timelineItems = generateTimelineItems()
 </script>
 
 <template>
     <div class='mt-7'>
         <ul>
-            <li v-for='{ hour } in timelineItems' 
-            :key='hour'
-            class='relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4'>
-                <a href="#" class='absolute -top-4 left-1/2 -translate-x-1/2 rouned bg-gray-100 px-2 font-mono text-lg text-gray-500'> 
-                    {{ hour }}:00
-                </a>
-            </li>
+            <TimelineItem 
+            v-for='timelineItem in timelineItems' 
+            :key='timelineItem.hour'
+            :timeline-item='timelineItem'
+            />
         </ul>
     </div>
 </template>
