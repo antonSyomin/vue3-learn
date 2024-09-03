f1be991db133a212bb6e00738d2c94f527c33819
anton <seminanton2@gmail.com>
2024-09-03 13:51:08 +0300

    Создание модуля активностей

--- ontrack/src/App.vue
+++ ontrack/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, provide, readonly } from 'vue'
+import { ref, provide, readonly } from 'vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
@@ -7,45 +7,27 @@ import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 
+import { generateTimelineItems, generatePeriodSelectOptions } from './functions'
+
 import {
-  generateActivities,
-  generateTimelineItems,
-  generateActivitySelectOptions,
-  generatePeriodSelectOptions
-} from './functions.js'
+  setActivitySecondsToComplete,
+  activitySelectOptions,
+  createActivity,
+  deleteActivity,
+  activities
+} from './activities'
 
 import { currentPage, timelineRef } from './router'
 import * as keys from './keys'
 
-const activities = ref(generateActivities())
 const timelineItems = ref(generateTimelineItems(activities.value))
-
-const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
-
-function deleteActivity(activity) {
-  timelineItems.value.forEach((timelineItem) => {
-    if (timelineItem.activityId === activity.id) {
-      timelineItem.activityId = null
-      timelineItem.activitySeconds = 0
-    }
-  })
-  activities.value.splice(activities.value.indexOf(activity), 1)
-}
 
 function setTimelineItemActivity(timelineItem, activityId) {
   timelineItem.activityId = activityId
 }
 
-function createActivity(activity) {
-  activities.value.push(activity)
-}
-
 function updateTimelineItemActivitySeconds(timelineItem, activitySeconds) {
   timelineItem.activitySeconds += activitySeconds
-}
-
-function setActivitySecondsToComplete(activity, secondsToComplete) {
-  activity.secondsToComplete = secondsToComplete || 0
 }
 
 provide(keys.updateTimelineItemActivitySecondsKey, updateTimelineItemActivitySeconds)
