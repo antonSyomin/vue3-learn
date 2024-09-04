fd23f2303a6e6a85a38cc11fd5f313b2576ca0af
anton <seminanton2@gmail.com>
2024-09-04 14:34:15 +0300

    Рефакторинг компонента ActivitySecondsToComplete

--- ontrack/src/components/ActivityItem.vue
+++ ontrack/src/components/ActivityItem.vue
@@ -7,7 +7,7 @@ import { ICON_TRASH } from '../icons'
 import { isActivityValid } from '../validators'
 import { updateActivity, deleteActivity } from '../activities'
 import { timelineItems, resetTimelineItemActivities } from '../timeline-items'
-import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
+import RemainingActivitySeconds from './RemainingActivitySeconds.vue'
 
 defineProps({
   activity: {
@@ -38,7 +38,7 @@ function deleteAndResetActivity(activity) {
         :selected="activity.secondsToComplete || null"
         @select="updateActivity(activity, { secondsToComplete: $event || 0 })"
       />
-      <ActivitySecondsToComplete v-if="activity.secondsToComplete" :activity="activity" />
+      <RemainingActivitySeconds v-if="activity.secondsToComplete" :activity="activity" />
     </div>
   </li>
 </template>
--- ontrack/src/functions.js
+++ ontrack/src/functions.js
@@ -47,6 +47,10 @@ export function currentHour() {
   return new Date().getHours()
 }
 
+export function formatSecondsWithSign(seconds) {
+  return `${seconds >= 0 ? '+' : '-'}${formatSeconds(seconds)}`
+}
+
 export function formatSeconds(seconds) {
   const date = new Date()
 
