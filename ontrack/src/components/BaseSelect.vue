3158b301e3564a5b494dcc594573970b0d27daf1
anton <seminanton2@gmail.com>
2024-09-04 10:39:30 +0300

    Модуль Icons

--- ontrack/src/components/BaseSelect.vue
+++ ontrack/src/components/BaseSelect.vue
@@ -5,6 +5,7 @@ import BaseIcon from './BaseIcon.vue'
 import { validateSelectOptions, isUndefinedOrNull, isSelectValueValid } from '../validators.js'
 import { BUTTON_TYPE_NEUTRAL } from '../constants'
 import { normalizeSelectValue } from '../functions'
+import { ICON_X_MARK } from '../icons'
 
 const props = defineProps({
   selected: [String, Number],
@@ -34,10 +35,10 @@ function select(value) {
 <template>
   <div class="flex gap-2">
     <BaseButton :type="BUTTON_TYPE_NEUTRAL" @click="select(null)">
-      <BaseIcon name="XMark" class="h-8" />
+      <BaseIcon :name="ICON_X_MARK" class="h-8" />
     </BaseButton>
     <select
-      class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl"
+      class="w-full truncate rounded bg-gray-100 px-2 py-1 text-2xl"
       @change="select($event.target.value)"
     >
       <option :selected="isNotSelected" disabled value="">
--- ontrack/src/components/TheActivityForm.vue
+++ ontrack/src/components/TheActivityForm.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, nextTick } from 'vue'
+import { ICON_PLUS } from '../icons'
 import { id } from '../functions'
 import { createActivity } from '../activities'
 import BaseButton from './BaseButton.vue'
@@ -26,7 +27,7 @@ async function submit() {
       v-model="name"
     />
     <BaseButton :disabled="name.trim() === ''">
-      <BaseIcon name="Plus" class="h-8" />
+      <BaseIcon :name="ICON_PLUS" class="h-8" />
     </BaseButton>
   </form>
 </template>
