6684d9087164c4e88462c839ae5b12887f629f37
anton <seminanton2@gmail.com>
2024-09-05 10:35:42 +0300

    Сохранение состояния приложения в локальном хранилище

--- ontrack/src/main.js
+++ ontrack/src/main.js
@@ -1,6 +1,22 @@
 import './assets/main.css'
+import * as storage from './storage'
+import { activities } from './activities'
+import { timelineItems } from './timeline-items'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') {
+    saveState()
+  }
+})
+
+function saveState() {
+  storage.save({
+    timelineItems: timelineItems.value,
+    activities: activities.value
+  })
+}
 
 createApp(App).mount('#app')
