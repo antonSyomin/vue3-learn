a1d28898b6926c92293c4155240b38fef9b5b499
anton <seminanton2@gmail.com>
2024-09-05 10:18:53 +0300

    Рефакторинг модуля time

---
+++ ontrack/src/time.js
@@ -0,0 +1,26 @@
+import { ref, computed } from 'vue'
+import { HUNDRED_PERCENT, SECONDS_IN_DAY, MILLISECONDS_IN_SECOND } from './constants'
+
+export const now = ref(new Date())
+
+export const secondsSinceMidnightInPercentage = computed(
+  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+)
+
+const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
+
+const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECOND)
+
+let timer = null
+
+export function startTimer() {
+  now.value = new Date()
+
+  timer = setInterval(() => {
+    now.value = new Date(now.value.getTime() + 5 * 60 * MILLISECONDS_IN_SECOND)
+  }, MILLISECONDS_IN_SECOND)
+}
+
+export function stopTimer() {
+  clearInterval(timer)
+}
