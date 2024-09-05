19310180651e6f2559ccd628b9ce84670cf7af66
anton <seminanton2@gmail.com>
2024-09-05 10:55:45 +0300

    Глобальный таймер для запуска секундомера при запуске приложения

--- ontrack/src/main.js
+++ ontrack/src/main.js
@@ -1,9 +1,16 @@
 import './assets/main.css'
 import { loadState, saveState } from './storage'
+import { startTimelineItemTimer, findActiveTimelineItem } from './timeline-items'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 loadState()
+
+const activeTimelineItem = findActiveTimelineItem()
+
+if (activeTimelineItem) {
+  startTimelineItemTimer(activeTimelineItem)
+}
 
 document.addEventListener('visibilitychange', () =>
   document.visibilityState === 'visible' ? loadState() : saveState()
